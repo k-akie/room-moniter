0a10bca86255197dc5662136bbce42c9017ea3f1
k-akie <kikuchiakie7@gmail.com>
2022-09-22 23:38:50 +0900

    README追加

---
+++ README.md
@@ -0,0 +1,9 @@
+# room-monitor
+BME680とArduino Yun Miniを使った部屋の環境モニタリングをするためのコード
+https://qiita.com/k-akie/items/9a3528c5345db045e061
+
+## フォルダ構成
+- arduino
+  - Arduino Yun Mini用のコード
+- functions
+  - Google Cloud Functions用のコード
--- functions/main.py
+++ functions/main.py
@@ -6,26 +6,17 @@ from google.cloud import bigquery
 
 
 @functions_framework.http
-def hello_http(request):
-    """HTTP Cloud Function.
-   Args:
-       request (flask.Request): The request object.
-       <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
-   Returns:
-       The response text, or any set of values that can be turned into a
-       Response object using `make_response`
-       <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-   """
+def register_monitoring_data(request):
     request_json = request.get_json(silent=True)
     dt_now_iso = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).isoformat()
     print(request_json, dt_now_iso)
 
     rows_to_insert = [{
         'createdAt': dt_now_iso,
-        'temperature': request_json['temperature'],  # 温度(*C)
+        'temperature': request_json['temperature'],  # 温度(℃)
         'pressure': request_json['pressure'],  # 気圧(hPa)
         'humidity': request_json['humidity'],  # 湿度(%)
-        'gas_resistance': request_json['gas_resistance'],  # (KOhms)
+        'gas_resistance': request_json['gas_resistance'],  # 気体抵抗(KOhms)
         'elevation': request_json['elevation']  # 標高(m)
     }]
 
