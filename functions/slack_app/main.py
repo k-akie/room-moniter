a6fd6b7d603e298aefe84ec261bc9bcb28dbef59
k-akie <kikuchiakie7@gmail.com>
2022-10-03 00:15:16 +0900

    Slack投稿Functionでスラッシュコマンドにも対応した

--- functions/slack_app/main.py
+++ functions/slack_app/main.py
@@ -1,14 +1,39 @@
 import datetime
+import logging
 import os
 
-import functions_framework
+from flask import Request
 from google.cloud import bigquery
+from slack_bolt import App, Ack, Respond
+from slack_bolt.adapter.google_cloud_functions import SlackRequestHandler
 from slack_sdk.web import WebClient
 
-jst = datetime.timezone(datetime.timedelta(hours=9), "JST")
+logging.basicConfig(level=logging.ERROR)
+app: App = App(process_before_response=True)
+handler = SlackRequestHandler(app)
 
 
-def send_slack(slack_api_token, channel, data):
+class Measurements:
+    createdAt: datetime.datetime
+    temperature: float
+    humidity: float
+    pressure: float
+    gas_resistance: float
+
+    def __init__(self, _created_at, _temperature, _humidity, _pressure, _gas_resistance):
+        self.createdAt = _created_at
+        self.temperature = _temperature
+        self.humidity = _humidity
+        self.pressure = _pressure
+        self.gas_resistance = _gas_resistance
+
+
+def make_slack_message(data: Measurements) -> str:
+    if not data:
+        return ""
+
+    jst = datetime.timezone(datetime.timedelta(hours=9), "JST")
+
     now_hour = datetime.datetime.now(jst).hour
     if now_hour > 17:
         greet = "こんばんは :stars: "
@@ -21,14 +46,19 @@ def send_slack(slack_api_token, channel, data):
     else:
         greet = ":sleeping_accommodation: ねないの？ :melting_face:"
 
-    message = f"{greet}\r{data}"
+    float_format = "6.1f"
+    measured_data = f"""
+> *{data.createdAt.astimezone(jst).strftime("%Y/%m/%d %H:%M")}*
+> 室温    {format(data.temperature, float_format)} ℃
+> 湿度    {format(data.humidity, float_format)} %
+> 気圧    {format(data.pressure, float_format)} hPa
+> 気体抵抗 {format(data.gas_resistance, float_format)} KOhms
+    """
 
-    client = WebClient(token=slack_api_token)
-    response = client.chat_postMessage(text=message, channel=channel)
-    print(response)
+    return f"{greet}\r{measured_data}"
 
 
-def search_bq(table_id):
+def search_bq(table_id) -> Measurements | None:
     client = bigquery.Client()
     query = f"""
         SELECT createdAt, temperature, humidity, gas_resistance, pressure
@@ -47,29 +77,41 @@ def search_bq(table_id):
     query_job = client.query(query, job_config=job_config)
 
     for row in query_job:
-        createdAt = row.createdAt.astimezone(jst).strftime("%Y/%m/%d %H:%M")
-        float_format = "6.1f"
-        return f"""
-> *{createdAt}*
-> 室温    {format(row.temperature, float_format)} ℃
-> 湿度    {format(row.humidity, float_format)} %
-> 気圧    {format(row.pressure, float_format)} hPa
-> 気体抵抗 {format(row.gas_resistance, float_format)} KOhms
-                """
-    return ""
+        return Measurements(
+            row.createdAt,
+            row.temperature,
+            row.humidity,
+            row.pressure,
+            row.gas_resistance
+        )
+    return
 
 
-@functions_framework.http
-def check_room(request):
+@app.command("/room-now")
+def command_room_now(ack: Ack, respond: Respond):
+    ack()
+
+    table_id = os.environ.get('BIGQUERY_TABLE_ID', 'Specified environment variable is not set.')
+    result = search_bq(table_id)
+    message = make_slack_message(result)
+    respond(message)
+
+
+# Cloud Function
+def entry_function(request: Request):
     request_json = request.get_json(silent=True)
 
     if request_json and 'channel' in request_json:
         channel = request_json['channel']
         table_id = os.environ.get('BIGQUERY_TABLE_ID', 'Specified environment variable is not set.')
-        slack_api_token = os.environ.get('SLACK_API_TOKEN', 'Specified environment variable is not set.')
+        slack_api_token = os.environ.get('SLACK_BOT_TOKEN', 'Specified environment variable is not set.')
 
         result = search_bq(table_id)
-        send_slack(slack_api_token, channel, result)
-        return 'OK'
+        message = make_slack_message(result)
 
-    return 'NG'
+        client = WebClient(token=slack_api_token)
+        response = client.chat_postMessage(text=message, channel=channel)
+        print(response)
+        return ""
+
+    return handler.handle(request)
