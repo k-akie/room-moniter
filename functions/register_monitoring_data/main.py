19b011741331267fc68185ad097d3ba732a5b208
k-akie <kikuchiakie7@gmail.com>
2023-01-28 17:30:35 +0900

    モニタリングデータ登録時に暑すぎたらSlack通知させる

--- functions/register_monitoring_data/main.py
+++ functions/register_monitoring_data/main.py
@@ -56,7 +56,45 @@ def warning_cold(temperature: float):
             'too_hot': False,
         }
     )
-    send_slack_daytime(f"${too_cold_temperature}度を下回りました :cold_face:")
+    send_slack_daytime(f"{too_cold_temperature}度を下回りました :cold_face:")
+
+
+def warning_hot(temperature: float):
+    too_hot_temperature = 30
+
+    db = firestore.Client()
+    latest_ref = db.collection('room-monitor').document('warning')
+    latest_dic = latest_ref.get().to_dict()
+
+    # 暑すぎリセット
+    if temperature < too_hot_temperature - 3:
+        latest_ref.update(
+            {
+                'updated': firestore.SERVER_TIMESTAMP,
+                'temperature': temperature,
+                'too_hot': False,
+            }
+        )
+        return
+
+    # 暑すぎない
+    if temperature < too_hot_temperature:
+        return
+
+    # ずっと暑すぎ
+    if latest_dic['too_hot']:
+        return
+
+    # 暑すぎ
+    latest_ref.update(
+        {
+            'updated': firestore.SERVER_TIMESTAMP,
+            'temperature': temperature,
+            'too_cold': False,
+            'too_hot': True,
+        }
+    )
+    send_slack_daytime(f"{too_hot_temperature}度を上回りました :hot_face:")
 
 
 def insert_bq(dt_now_iso, request_json) -> bool:
@@ -105,6 +143,7 @@ def register_monitoring_data(request):
     if insert_bq(dt_now_iso, request_json):
         update_latest_fs(dt_now, request_json)
         warning_cold(request_json['temperature'])
+        warning_hot(request_json['temperature'])
         return 'OK {}'.format(dt_now_iso)
 
     return 'NG {}'.format(dt_now_iso)
